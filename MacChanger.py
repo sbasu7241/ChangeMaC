74c1da4403c93bc05d7ed70cb1545b18055ab058
sbasu7241 <42107347+sbasu7241@users.noreply.github.com>
2019-09-22 19:51:09 +0530

    Remove CLI vulnerability

--- MacChanger.py
+++ MacChanger.py
@@ -7,13 +7,10 @@ new_mac = "00:11:22:33:44:55"
 interface = input("interface > ")
 new_mac = input("new MAC > ")
 
-print(interface)
-print(new_mac)
-
 print("[+] Changing MAC address for "+interface+" to "+ new_mac)
 
-subprocess.call("sudo ifconfig "+interface+" down",shell=True)
-subprocess.call("sudo ifconfig "+interface+" hw ether "+new_mac,shell=True)
-subprocess.call("sudo ifconfig "+interface+" up",shell=True)
+subprocess.call(["sudo","ifconfig",interface,"down"])
+subprocess.call(["sudo","ifconfig",interface,"hw","ether",new_mac])
+subprocess.call(["sudo","ifconfig",interface,"up"])
 
 
