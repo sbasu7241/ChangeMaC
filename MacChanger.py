a9fdadc742f72e1a0b47c1b3c21611b71bef56a7
sbasu7241 <42107347+sbasu7241@users.noreply.github.com>
2019-09-23 02:11:59 +0530

    Final codes uploaded

--- MacChanger.py
+++ MacChanger.py
@@ -2,6 +2,7 @@
 #!/usr/bin/env/python
 import subprocess
 from optparse import OptionParser
+import re
 
 def change_mac(interface,new_mac):
 	print("[+] Changing MAC address for "+interface+" to "+ new_mac)
@@ -9,7 +10,16 @@ def change_mac(interface,new_mac):
 	subprocess.call(["sudo","ifconfig",interface,"down"])
 	subprocess.call(["sudo","ifconfig",interface,"hw","ether",new_mac])
 	subprocess.call(["sudo","ifconfig",interface,"up"])
+	
+def get_current_mac(interface):
+	ifconfig_result = subprocess.check_output(['ifconfig',interface]).decode('utf-8')
 
+	mac_address_search_result = re.search(r"\w\w:\w\w:\w\w\:\w\w:\w\w:\w\w",ifconfig_result)
+
+	if mac_address_search_result:
+		return mac_address_search_result.group(0)
+	else:
+		print("[-] Could not read MAC address")
 
 def get_arguments():
 	parser = OptionParser()
@@ -17,8 +27,41 @@ def get_arguments():
 	parser.add_option("-i","--interface",dest="interface",help="Inteface to change MAC Address")
 	parser.add_option("-m","--mac",dest="new_mac",help="New MAC Address")
 
-	return parser.parse_args()
+	(options,arguments) = parser.parse_args()
+	if not options.interface:
+		parser.error("[-] Please specify an interface, use --help for more info.")
+	if not options.new_mac:
+		parser.error("[-] Please specify a mac address, use --help for more info.")
+	return options
 
-(options,arguments) = get_arguments()
-change_mac(options.interface,options.new_mac)
+def start():
+
+	options = get_arguments()
+
+
+	current_mac = get_current_mac(options.interface)
+	print("Current MAC = "+str(current_mac))
+
+	change_mac(options.interface,options.new_mac)
+	current_mac = get_current_mac(options.interface)
+
+	if current_mac == options.new_mac:
+		print("[+] MAC address successfully changed to "+current_mac)
+	else:
+		print("[-] MAC address did not get changed ")
+
+if __name__ == "__main__":
+	start()
+
+
+
+
+
+
+
+
+
+
+
+
 
