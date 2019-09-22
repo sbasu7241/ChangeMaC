d8c9d8378b49a3e84f3bb3334c27f56e1e982282
sbasu7241 <42107347+sbasu7241@users.noreply.github.com>
2019-09-22 19:39:21 +0530

    Updated

--- MacChanger.py
+++ MacChanger.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env/python
 import subprocess
 
-subprocess.call("sudo ifconfig wlo1 down",shell=True)
-subprocess.call("sudo ifconfig wlo1 hw ether 00:11:22:33:44:55",shell=True)
-subprocess.call("sudo ifconfig wlo1 up",shell=True)
+interface = "wlo1"
+new_mac = "00:11:22:33:44:55"
+
+interface = input("interface > ")
+new_mac = input("new MAC > ")
+
+print(interface)
+print(new_mac)
+
+print("[+] Changing MAC address for "+interface+" to "+ new_mac)
+
+subprocess.call("sudo ifconfig "+interface+" down",shell=True)
+subprocess.call("sudo ifconfig "+interface+" hw ether "+new_mac,shell=True)
+subprocess.call("sudo ifconfig "+interface+" up",shell=True)
 
 
